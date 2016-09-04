7f6b3426231ac4b5481efca29e750d91667e9b62
Olivier <olivier.brabanders@gmail.com>
2016-09-04 20:01:38 +0200

    Add a first unittest to the 'products' app
    The added unittest is a simple one, but for now it should do.

--- ingredient_viewer/products/tests.py
+++ ingredient_viewer/products/tests.py
@@ -2,8 +2,18 @@ from django.test import TestCase
 
 
 # Create your tests here.
+from .models import Product
+from .factories import ProductFactory
+
+
 class ProductTest(TestCase):
     """
     This class represents unittests for the Products model.
     """
-    pass
+    def test_create_product(self):
+        """ Test if a product can be added to and retrieved from the database. """
+        test_name = 'testproduct'
+        test_product = ProductFactory(product_name=test_name)
+
+        saved_product = Product.objects.get(product_name=test_name)
+        self.assertEqual(test_product.pk, saved_product.pk)
